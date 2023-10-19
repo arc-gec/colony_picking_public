d5802d381b7a9cd3da3e84e3e1bd9d29ddec3c97
Yarrow Madrona <yarrowm@ArcResearch78.local>
2023-10-19 07:44:34 -0700

    Added Machine readable barcodes

--- colony_picking_app.py
+++ colony_picking_app.py
@@ -5,16 +5,19 @@ import datetime
 from barcode import Code39
 import os
 import sys
+from io import BytesIO
+from PIL import Image
 
 #import path
 #PATH = os.getcwd()+'/pages'
 #sys.path.append(PATH)
 #Import modules
-from plate_picking import make_plate_list, generate_barcode, get_table_map
+from plate_picking import make_plate_list, generate_barcode, machine_barcode, get_table_map, convert_images_html
 from streamlit_helper_functions import convert_df
 
 #Global Variables. Will import these properly when I have time.
-QPIX_DICT = {'A1': 'A1','B1': 'A2','C1': 'A3','D1': 'A4','E1': 'A5','F1': 'A6',
+#For dict below key is qpix and value is human
+QPIX_DICT_90 = {'A1': 'A1','B1': 'A2','C1': 'A3','D1': 'A4','E1': 'A5','F1': 'A6',
  	     'A2': 'B1', 'B2': 'B2', 'C2': 'B3', 'D2': 'B4', 'E2': 'B5', 'F2': 'B6',
  	     'A3': 'C1', 'B3': 'C2', 'C3': 'C3', 'D3': 'C4', 'E3': 'C5', 'F3': 'C6',
  	     'A4': 'D1', 'B4': 'D2', 'C4': 'D3', 'D4': 'D4', 'E4': 'D5', 'F4': 'D6',
@@ -24,9 +27,21 @@ QPIX_DICT = {'A1': 'A1','B1': 'A2','C1': 'A3','D1': 'A4','E1': 'A5','F1': 'A6',
              'A8': 'H1', 'B8': 'H2', 'C8': 'H3', 'D8': 'H4', 'E8': 'H5', 'F8': 'H6'
 	    }
 
-def convert_qpix_wells(df):
-	df = df.rename(columns={'Source Well': 'Qpix Source Well'})
-	df['Human Source Well'] = df['Qpix Source Well'].map(QPIX_DICT)
+QPIX_DICT_180 = {'H6': 'A1', 'H5': 'A2', 'H4': 'A3', 'H3': 'A4', 'H2': 'A5', 'H1': 'A6', 
+		'G6': 'B1', 'G5': 'B2', 'G4': 'B3', 'G3': 'B4', 'G2': 'B5', 'G1': 'B6',
+ 		'F6': 'C1', 'F5': 'C2', 'F4': 'C3', 'F3': 'C4', 'F2': 'C5', 'F1': 'C6', 
+		'E6': 'D1', 'E5': 'D2', 'E4': 'D3', 'E3': 'D4', 'E2': 'D5', 'E1': 'D6', 
+		'D6': 'E1', 'D5': 'E2', 'D4': 'E3', 'D3': 'E4', 'D2': 'E5', 'D1': 'E6', 
+		'C6': 'F1', 'C5': 'F2', 'C4': 'F3', 'C3': 'F4', 'C2': 'F5', 'C1': 'F6', 
+		'B6': 'G1', 'B5': 'G2', 'B4': 'G3', 'B3': 'G4', 'B2': 'G5', 'B1': 'G6', 
+		'A6': 'H1', 'A5': 'H2', 'A4': 'H3', 'A3': 'H4', 'A2': 'H5', 'A1': 'H6'
+}	
+
+ 
+def convert_qpix_wells(df, qpix_rot_dict):
+	print(df.columns)
+	df = df.rename(columns={'Source Region': 'Qpix Source Well'})
+	df['Human Source Well'] = df['Qpix Source Well'].map(qpix_rot_dict)
 	df = df[['Source Barcode', 'Destination Barcode', 'Qpix Source Well', 'Human Source Well', 'Destination Well']]
 	return df
 
@@ -68,8 +83,8 @@ def main():
 			df_list = make_plate_list(df)
 			for dframe in df_list:
 				df2 = generate_barcode(dframe, plate_type = 'qtrey')
-				st.write(df2)
-				
+				df2_html = df2.to_html(escape=False, formatters = {'Source Agar Plate Barcode_machine':convert_images_html})
+				st.write(df2_html, unsafe_allow_html=True)
 			st.download_button(
 			label = "Download qtrey input as CSV",
 			data = convert_df(df2),
@@ -77,10 +92,27 @@ def main():
 			mime = "text/csv",
 			)
 
+			unique_source_barcodes = df2['Source Agar Plate Barcode_image_path'].unique().tolist()
+			unique_destination_barcodes = df2['Destination 96 Plate Barcode_image_path'].unique().tolist()
+			print(unique_source_barcodes)
+			
+			for i in unique_source_barcodes:
+				st.image(i, caption='Source Agar Plate Barcode')
+				with open(i, "rb") as file:
+					btn = st.download_button(
+							'Download plate barcode',
+							data=file,
+							file_name='Source Barcode', 
+							key=i, 
+							mime="image/jpeg")
+			for i in unique_destination_barcodes:
+				st.image(i, caption='Destination Plate Barcode')
+				st.download_button('Download plate barcode',i, key=i, mime="image/jpeg") 
+
 		upload_file2 = st.file_uploader(label="Please Upload Qpix qtrey picking output", key=3)
 		if upload_file2 is not None:
-			df3 = pd.read_csv(upload_file2, header=11)
-			df3 = convert_qpix_wells(df3)
+			df3 = pd.read_csv(upload_file2, header=6)
+			df3 = convert_qpix_wells(df3, QPIX_DICT_180)
 			st.write(df3)
 
 			st.download_button(
--- plate_picking.py
+++ plate_picking.py
@@ -1,10 +1,16 @@
+import os
 import pandas as pd
 import streamlit as st
 import plotly.figure_factory as ff 
 import plotly_express as px
 import plotly.offline as pyo
 import datetime
+#import barcode
 from barcode import Code39
+from barcode.writer import ImageWriter
+from io import BytesIO
+from PIL import Image
+import base64
 
 def rowIndex(row):
 	return row.name
@@ -15,8 +21,23 @@ def make_plate_list(df):#Tests if there are more than 96 colonies
 	if df['No. Colonies'].sum() < 97:
 		return [df]	
 	else:
-		pass
-		
+		print("Still working on multiple plates")
+
+# Converting links to html tags
+def convert_images_html(path_to_image):
+    return f'<img src=path_to_image width = 60 >'
+
+def machine_barcode(human_bc: str):
+	image_path = os.getcwd()+'/barcode_images/'+human_bc+'.jpg'
+	#code39 = barcode.get_barcode_class('code39')
+	my_code39 = Code39(human_bc, writer=ImageWriter())
+	img_bytes = BytesIO()
+	my_code39.write(img_bytes)
+	img = Image.open(img_bytes)
+	img.save(image_path, "JPEG")
+	return (image_path)
+
+
 def generate_barcode(df, plate_type):
 	
 	date_time = datetime.datetime.now()
@@ -30,17 +51,32 @@ def generate_barcode(df, plate_type):
 		df['Source Agar Plate No.'] = 1
 	
 	df['Source Agar Plate No.'] = df['Source Agar Plate No.'].astype(str)
-	df['Source Agar Plate Barcode'] = (df['Source Barcode']+(df['Source Agar Plate No.'])).apply(lambda x: Code39(x))
-	df['Destination 96 Plate Barcode'] = Code39(barcode.replace('source', 'dest'))
+	df['Source Agar Plate Barcode'] = (df['Source Barcode']+df['Source Agar Plate No.']).str.upper() # .apply(lambda x: Code39(x))
+	df['Destination 96 Plate Barcode'] = barcode.replace('source', 'dest').upper()
 	df_stamp = df.copy()
-	df_stamp['Destination 96 Plate Barcode'] = Code39(barcode.replace('source', 'stamp'))
+	df_stamp['Destination 96 Plate Barcode'] = barcode.replace('source', 'stamp').upper()
 	df = pd.concat([df, df_stamp], axis = 0, ignore_index=True)
 	
 	if plate_type == 'petri': 
 		df = df[['Name', 'Source Agar Plate Name', 'Source Agar Plate No.', 'No. Colonies', 'Source Agar Plate Barcode', 'Destination 96 Plate Barcode']]  
 	elif plate_type == 'qtrey':
 		df = df[['Name', 'Source Agar Plate Name', 'Source Agar Plate No.','Source Well', 'No. Colonies', 'Source Agar Plate Barcode', 'Destination 96 Plate Barcode']]
+	
+	for i in ['Source Agar Plate Barcode', 'Destination 96 Plate Barcode']:	
+		d = {}
+		d_image = {}
+		for human_barcode in df[i].unique().tolist():
+			img_path = machine_barcode(human_barcode)
+			print(img_path, human_barcode)
+			d_image[human_barcode] = img_path
+			html_path = convert_images_html(img_path)
+			d[human_barcode] = html_path
+			print(d)
+		df[i+'_image_path'] = df[i].map(d_image)	
+		df[i+'_machine'] = df[i].map(d)
+	
 	return df
+
 
 def get_table_map(df):
 	df['row'] = df['Destination Well'].apply(lambda x: x[0])
@@ -65,3 +101,5 @@ def get_table_map(df):
 	fig = fig.update_yaxes(showgrid=False)
 	
 	return fig
+
+
